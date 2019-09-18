453192bf0779397e906b51760305424a52f0246e
sandip bamrel <49833723+whatevea@users.noreply.github.com>
2019-09-18 21:32:32 +0545

    Update index.py

--- index.py
+++ index.py
@@ -1,12 +1,11 @@
-from flask import Flask,Response
+from flask import Flask,request
 app= Flask(__name__)
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
 def catch_all(path):
     return Response("<h1>Flask on Now</h1><p>You visited: /%s</p>" % (path), mimetype="text/html")
 @app.route('/')
 def nulljson():
-	return "NO request"
+	gc=requests.args['g']
+	return "NO request"+ "gogoanimecode sent is "+gc
 @app.route('/anime/<string:name>',methods=['POST'])
 def findallepisode():
 	pass
