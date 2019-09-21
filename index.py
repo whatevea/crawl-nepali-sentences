45e63466856e9729c759d0dde3ce9536af1c1783
sandip bamrel <49833723+whatevea@users.noreply.github.com>
2019-09-21 14:07:38 +0545

    Update index.py

--- index.py
+++ index.py
@@ -1,9 +1,9 @@
 from flask import Flask,request,jsonify
-from bs4 import BeautifulSoup as bs
+from bs4 import BeautifulSoup,SoupStrainer
 import requests
 app= Flask(__name__)
 def catch_all(path):
-    return Response("<h1>Flask on Now</h1><p>You visited: /%s</p>" % (path), mimetype="text/html")
+    return Response("<h1>Flask on Now</h1><p>Fuck you , you cant access: /%s</p>" % (path), mimetype="text/html")
 @app.route('/')
 def nulljson():
 
@@ -12,12 +12,24 @@ def nulljson():
 	gc=request.args['g']
 	url="https://www.gogoanime1.com/watch/"+gc
 	resp=requests.get(url).text
-	soup=bs(resp,'html.parser')
-	soup=soup.find_all('div',{'class':'ci-ct tnContent'})[1]
-	total=soup.find_all('li')
+	strainerobj=SoupStrainer('div',{'class':'ci-ct tnContent'})
+	soup=BeautifulSoup(resp,'html.parser',parse_only=strainerobj)
+	print(soup)
+	total=soup.find_all('div',{'class':'ci-ct tnContent'})[1].find_all('li')
 	for items in total:
-		c={items.a['href']:items.a['href'].rsplit('/',1)[1]}
+		link=items.a['href']
+		newreq=requests.get(link).text
+		# newbsobj=BeautifulSoup(newreq,'html.parser')
+		secondstobj=SoupStrainer('a')
+		ssoup=BeautifulSoup(newreq,'html.parser',parse_only=secondstobj)
+		videolink=ssoup.find('a',text="Download")['href']
+		print(videolink)
+		c={items.a['href'].rsplit('/',1)[1]:videolink}
+		# c={items.a['href']:items.a['href'].rsplit('/',1)[1]}
 		epsiodelist.append(c)
+
+	# epsiodelist=[dict(t) for t in {tuple(sorted(d.items())) for d in epsiodelist}] #filtering duplicates due to two 
+	epsiodelist.reverse()
 	response=jsonify({"data":epsiodelist})
 	response.headers.add('Access-Control-Allow-Origin', '*')
 	response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
