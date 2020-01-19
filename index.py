1d30f7f9a78cdfd66e9b92f34e14106faf10d30c
sandip bamrel <49833723+whatevea@users.noreply.github.com>
2020-01-19 18:48:57 +0545

    Update index.py

--- index.py
+++ index.py
@@ -1,38 +1,60 @@
-from flask import Flask,request,jsonify
-from bs4 import BeautifulSoup,SoupStrainer
+from flask import Flask,jsonify,request
 import requests
-app= Flask(__name__)
+import re
+from bs4 import BeautifulSoup as bs
+app=Flask(__name__)
+app.config['DEBUG']=True
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
 def catch_all(path):
-    return Response("<h1>Flask on Now</h1><p>Fuck you , you cant access: /%s</p>" % (path), mimetype="text/html")
-@app.route('/')
-def nulljson():
+	q=request.args.get('q')
+	url='https://vidstreaming.io/'+q
+	html=requests.get(url).text
+	try:
+		soup=bs(html,'html.parser')
+		src="https:"+soup.iframe['src']
+		page=requests.get(src).text
+		regex = r"'https://redirecto[^']+'"
+		matches = re.search(regex, page)
+		start=matches.start()
+		end=matches.end()
+		tag=f'''
+	<body>
+	  <video
+	    id="my-video"
+	    class="video-js"
+	    controls
+	    preload="auto"
+	    width="640"
+	    height="264"
+	    poster="videoposter.jpg"
+	    data-setup=""
+	  >
+	    <source src={page[start:end]} type="video/mp4" />
+	    <p class="vjs-no-js">
+	      To view this video please enable JavaScript, and consider upgrading to a
+	      web browser that
+	      <a href="https://videojs.com/html5-video-support/" target="_blank"
+	        >supports HTML5 video</a
+	      >
+	    </p>
+	  </video>
 
-	epsiodelist=[]
-	c={}
-	gc=request.args['g']
-	url="https://www.gogoanime1.com/watch/"+gc
-	resp=requests.get(url).text
-	strainerobj=SoupStrainer('div',{'class':'ci-ct tnContent'})
-	soup=BeautifulSoup(resp,'html.parser',parse_only=strainerobj)
-	total=soup.find_all('div',{'class':'ci-ct tnContent'})[1].find_all('li')
-	for items in total:
-		link=items.a['href']
-		newreq=requests.get(link).text
-		# newbsobj=BeautifulSoup(newreq,'html.parser')
-		secondstobj=SoupStrainer('a')
-		ssoup=BeautifulSoup(newreq,'html.parser',parse_only=secondstobj)
-		videolink=ssoup.find('a',text="Download")['href']
-		c={items.a['href'].rsplit('/',1)[1]:videolink}
-		# c={items.a['href']:items.a['href'].rsplit('/',1)[1]}
-		epsiodelist.append(c)
+	  <script src="https://vjs.zencdn.net/7.6.6/video.js"></script>
+ '''
+		tag=jsonify({"if":tag})
+		tag.headers.add('Access-Control-Allow-Origin', '*')
+		tag.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+		tag.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+		return tag
+	except:
+		iframe=html.find('<iframe')
+		iframe2=html.find('>',iframe)+1
+		notstripped=html[iframe:iframe2]
+		response=jsonify({"if":notstripped})
+		response.headers.add('Access-Control-Allow-Origin', '*')
+		response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+		response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+		return response
 
-	# epsiodelist=[dict(t) for t in {tuple(sorted(d.items())) for d in epsiodelist}] #filtering duplicates due to two 
-	epsiodelist.reverse()
-	response=jsonify({"data":epsiodelist})
-	response.headers.add('Access-Control-Allow-Origin', '*')
-	response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-	response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
-	return response
-@app.route('/anime/<string:name>',methods=['POST'])
-def findallepisode():
-	pass
+
