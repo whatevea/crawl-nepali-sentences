1b8031e83cb8654d5857ba1bea6c33ff082206b3
sandip bamrel <49833723+whatevea@users.noreply.github.com>
2020-01-21 21:35:16 +0545

    Update index.py

--- index.py
+++ index.py
@@ -8,13 +8,9 @@ def home():
 		url=request.args['url']
 		url="https://"+url.replace('^','/')
 		response= requests.get(url,headers=headers).text
-		response.headers.add('Access-Control-Allow-Origin', '*')
-		response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-		response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+		return response
 	else:
 		url=request.args['url']
 		url="https://"+url.replace('^','/')
 		response=requests.get(url).text
-		response.headers.add('Access-Control-Allow-Origin', '*')
-		response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-		response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+		retrun response
