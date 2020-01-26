a6bce64ec489b897385ffee9196a7183eba60b57
sandip bamrel <49833723+whatevea@users.noreply.github.com>
2020-01-26 21:00:53 +0545

    Update index.py

--- index.py
+++ index.py
@@ -15,9 +15,9 @@ def catch_all(path):
   soup=bs(html,'html.parser')
   php="https:"+soup.iframe['src']
   phppage=requests.get(php).text
-  try:
-    regex = r"'https://redirecto[^']+'"
-    matches = re.search(regex, phppage)
+  regex = r"'https://redirecto[^']+'"
+  matches = re.search(regex, phppage)
+  if matches:
     start=matches.start()
     end=matches.end()
     videourl=phppage[start:end]
@@ -36,19 +36,21 @@ def catch_all(path):
       <source src={videourl} type="video/mp4" />
     </video>
  '''
-  except:
-    soup=bs(phppage,'html.parser')
-    xtream=soup.find('li',text='Xstreamcdn')['data-video'].replace('/','^')
-    videourl='https://crawl-nepali-sentences-git-iframewithoutsandbox.sandipbamrel.now.sh/?link='+xtream
-    notstripped=f'<iframe id="embed-responsive-item" src="{videourl}" marginwidth=0 marginheight=0 scrolling="no" width="640" height="500" allowfullscreen="true" frameborder="0" scrolling="no" sandbox="allow-scripts allow-same-origin"></iframe>'
   else:
-    iframe=html.find('<iframe')
-    iframe2=html.find('>',iframe)+1
-    notstripped=html[iframe:iframe2]
-    if notstripped=="":
-      notstripped="<h2>Episode not valid or is not yet available</h2>"
+  	try:
+  		soup=bs(phppage,'html.parser')
+  		xtream=soup.find('li',text='Xstreamcdn')['data-video'].replace('/','^')
+  		videourl='https://crawl-nepali-sentences-git-iframewithoutsandbox.sandipbamrel.now.sh/?link='+xtream
+  		notstripped=f'<iframe id="embed-responsive-item" src="{videourl}" marginwidth=0 marginheight=0 scrolling="no" width="640" height="500" allowfullscreen="true" frameborder="0" scrolling="no" sandbox="allow-scripts allow-same-origin"></iframe>'
+  	except:
+  		iframe=html.find('<iframe')
+  		iframe2=html.find('>',iframe)+1
+  		notstripped=html[iframe:iframe2]
+  		if notstripped=="":
+  			notstripped="<h2>Episode not valid or is not yet available</h2>"
   response=jsonify({"if":notstripped})
   response.headers.add('Access-Control-Allow-Origin', '*')
   response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
   response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
   return response
+
