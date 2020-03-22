18adacee51b974a37909e65ba2d7e0c9af218e5f
sandip bamrel <49833723+whatevea@users.noreply.github.com>
2020-03-22 12:29:45 +0545

    flask database test

--- index.py
+++ index.py
@@ -1,38 +1,19 @@
-from flask import Flask,request,jsonify
-from bs4 import BeautifulSoup,SoupStrainer
-import requests
-app= Flask(__name__)
-def catch_all(path):
-    return Response("<h1>Flask on Now</h1><p>Fuck you , you cant access: /%s</p>" % (path), mimetype="text/html")
+from flask import Flask,request
+from database_module import insert,create_me
+app=Flask(__name__)
 @app.route('/')
-def nulljson():
-
-	epsiodelist=[]
-	c={}
-	gc=request.args['g']
-	url="https://www.gogoanime1.com/watch/"+gc
-	resp=requests.get(url).text
-	strainerobj=SoupStrainer('div',{'class':'ci-ct tnContent'})
-	soup=BeautifulSoup(resp,'html.parser',parse_only=strainerobj)
-	total=soup.find_all('div',{'class':'ci-ct tnContent'})[1].find_all('li')
-	for items in total:
-		link=items.a['href']
-		newreq=requests.get(link).text
-		# newbsobj=BeautifulSoup(newreq,'html.parser')
-		secondstobj=SoupStrainer('a')
-		ssoup=BeautifulSoup(newreq,'html.parser',parse_only=secondstobj)
-		videolink=ssoup.find('a',text="Download")['href']
-		c={items.a['href'].rsplit('/',1)[1]:videolink}
-		# c={items.a['href']:items.a['href'].rsplit('/',1)[1]}
-		epsiodelist.append(c)
-
-	# epsiodelist=[dict(t) for t in {tuple(sorted(d.items())) for d in epsiodelist}] #filtering duplicates due to two 
-	epsiodelist.reverse()
-	response=jsonify({"data":epsiodelist})
-	response.headers.add('Access-Control-Allow-Origin', '*')
-	response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-	response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
-	return response
-@app.route('/anime/<string:name>',methods=['POST'])
-def findallepisode():
-	pass
+def homepage():
+	return "<a href='/create'>Create database here</a>"
+@app.route('/create')
+def create():
+	try:
+		create_me()
+		return "database created nigga"
+	except:
+		return "table already exist"
+@app.route('/insert',methods=['POST'])
+def insert_data():
+	name=request.form['name']
+	age=request.form['age']
+	insert(name,age)
+	return f'{str(name)} of age {age} inserteds'
